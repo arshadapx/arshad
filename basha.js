3cc220aa64999c0cdb15877ddce6fc7843835fba
arshadapx <arshadappxcube.com>
2022-11-07 12:54:35 +0530

    first commit

---
+++ basha.js
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>javascript syntax</title>
+	<script type="text/javascript">
+		document.w 
+	</script>
+</head>
+<body>
+	<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+</body>
+</html>    
